4e3ea4f87d9f31ccd5931689afe0a48db4e9ffcd
Hood <hood@mit.edu>
2017-06-21 08:13:37 -0400

    Fixed header on README, CHANGELOG, and one other file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 Package: spectralsequences version 1.0.0
 Author: Hood Chatham
 Email: hood@mit.edu
-2017-06-20
+Date: 2017-06-20
 License: All files have the Latex Project Public License.
 File: CHANGELOG.md
 
@@ -10,4 +10,4 @@ File: CHANGELOG.md
 ### Fixed: 
 - The \tagclass command was broken. Now it works.
 - Scopes didn't nest properly, now they do.
-- Fixed a big performance issue with tmfass.tex related to nesting scopes and shifts. 
+- Fixed a big performance issue with nested scopes and shifts. 
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 echo "Compiling manual"
 cd manual
 texify --pdf --quiet spectralsequencesmanual.tex
@@ -22,14 +22,17 @@ echo "Cleanup tex output files"
 echo "Replacing date strings"
 grep -lr "Package: spectralsequences.sty version" * | grep -vE "sh$" | xargs sed --in-place "s/Date: ....-..-../Date: $(date +%Y-%m-%d)/g"
 
-
 # If a version number was supplied, replace versions:
 #grep -l "Date: 2017-06-21" | xargs sed --in-place "s/Date: 2017-06-21/Date: $(date +%Y-%m-%d)/g"
 
 echo "Zipping files"
-powershell "Get-ChildItem . -r -Exclude ".git","*.sh","*.zip","*.txt" | Write-Zip -OutputPath spectralsequences.zip"
+powershell "Get-ChildItem . -r -Exclude ".git","*.sh","*.zip","*.txt" | Write-Zip -OutputPath spectralsequences.zip | Out-Null"
 
 if [ -s commitmessage.txt ] ; then
+    echo "Committing" 
     git commit -a --file commitmessage.txt
+    mv commitmessage.txt commitmessage.bak.txt
     printf "" > commitmessage.txt
+    echo "Pushing commit"
+    git push
 fi
