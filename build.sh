cd9d896b1048424b93774364387da64457b0761c
Hood Chatham <hood@mit.edu>
2017-07-01 16:56:17 -0400

    Speed improvements, fixed title/labels, made "label handler" able to handle single command, added time test info to repo.

--- build.sh
+++ build.sh
@@ -11,7 +11,8 @@ fi
 cd ..
 
 echo "Compiling examples"
-./compileexamples.sh
+./compileexamples.sh | tee timetest.txt 
+sed -r --in-place "s/\x1B\[([0-9]{1,3}((;[0-9]{1,3})*)?)?[m|K]//g" timetest.txt
 if [ $? -gt 0 ] ; then
     echo "Examples failed; quitting."
     exit 1
--- compileexamples.sh
+++ compileexamples.sh
@@ -6,9 +6,8 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-
 for file in $(ls -1 *.tex); do
-  printf $file
+  printf "$file"
   out=( $( ( /usr/bin/timeout 100 /usr/bin/time -f %e pdflatex --quiet $file 2>&1; echo $? ) | tr -d '\0' ) )
   if [ -z ${out[0]+x} ]; then
       allPassed=0
