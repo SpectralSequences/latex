00459b86d5f63c8e2a9140b59b39437121ad599c
Hood <hood@mit.edu>
2021-07-17 18:37:16 -0700

    Improve stripping code

--- build.lua
+++ build.lua
@@ -25,16 +25,8 @@ function my_normalize_log(content, engine, errlevels)
     for line in string.gmatch(new_content, "([^\n]*)\n") do
         if pattern then
             if line == "END_EXPECT_THROWS" then
-                if string.match(cur_expect_block, pattern) then
-                    result = result .. "expect match succeeded: pattern '" .. pattern .. "' was found." .. os_newline
-                else
-                    print("!!!!!!!!!!!!!!!!!!\n")
-                    print("expect match failed: pattern '" .. pattern .. "' not found in block:")
-                    print("")
-                    print(cur_expect_block)
-                    print("!!!!!!!!!!!!!!!!!!\n")
-                    result = result .. "expect match failed: pattern '" .. pattern .. "' not found." .. os_newline
-                end
+                output = handle_expect_block(cur_expect_block, pattern)
+                result = result .. output .. os_newline
                 pattern = nil
                 cur_expect_block = ""
             else
@@ -43,7 +35,8 @@ function my_normalize_log(content, engine, errlevels)
         else
             pattern = string.match(line, "EXPECT_THROWS::(.*)::$")
             if not pattern then
-                if not string.match(line, "^[.]+\\") then
+                line = clean_line(line)
+                if line then
                     result = result .. line .. os_newline
                 end
             end
@@ -51,6 +44,27 @@ function my_normalize_log(content, engine, errlevels)
 
     end
 	return result
+end
+
+function clean_line(line)
+    if not string.match(line, "^[.]+\\") then
+        return nil
+    end
+    line = string.gsub(line, "[(][0-9.]*pt too [a-z]*[)]", "")
+    return line
+end
+
+function handle_expect_block(expect_block, pat)
+    if string.match(expect_block, pat) then
+        return "expect match succeeded: pattern '" .. pat .. "' was found."
+    else
+        print("!!!!!!!!!!!!!!!!!!\n")
+        print("expect match failed: pattern '" .. pat .. "' not found in block:")
+        print("")
+        print(expect_block)
+        print("!!!!!!!!!!!!!!!!!!\n")
+        return "expect match failed: pattern '" .. pat .. "' not found."
+    end
 end
 
 
